c271f10ccd3fe9cc33200b6b1d05f57c6aa352fe
cs60181888 <qwerkdud1999@gmail.com>
2020-07-08 14:01:30 +0900

    finish

---
+++ SpaceInvaders/main.py
@@ -0,0 +1,55 @@
+import pygame
+import os
+import time
+import random
+pygame.font.init()
+
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter Tutorial")
+
+
+# Load images
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+# Player ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small.png"))
+
+# Lasers
+RED_LASER = pygame.image.load(os.path.join("assets", "pixer_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixer_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixer_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixer_laser_yellow.png"))
+
+# Background
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+
+def main():
+    run = True
+    FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
+
+    clock = pygame.time.Clock()
+    
+    def redraw_window():
+        WIN.blit(BG, (0,0))
+        # draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255)) # f 는 안에 변수가 들어간다는 것을 나타낸다.
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_Width() - 10, 10))
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+main()
