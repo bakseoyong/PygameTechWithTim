75ff3e64f2565482458c699515f4e921eca8a316
cs60181888 <qwerkdud1999@gmail.com>
2020-07-09 15:19:46 +0900

    third commit

--- main.py
+++ main.py
@@ -1,260 +1,125 @@
 import pygame
-import os
-import time
+import math
 import random
-pygame.font.init()
 
-WIDTH, HEIGHT = 750, 750
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space Shooter Tutorial")
+# set up
+pygame.init()
+WIDTH, HEIGHT = 800, 500
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Hangman Game!")
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# font
+LETTER_FONT = pygame.font.SysFont('comicsans', 40)
+WORD_FONT = pygame.font.SysFont('comicsans', 60)
+TITLE_FONT = pygame.font.SysFont('comicsans', 70)
+
+# load image
+images = []
+for i in range(1, 7):
+    image = pygame.image.load("images/hangman" + str(i) + ".png")
+    images.append(image)
+
+# button variables
+RADIUS = 20
+GAP = 15
+letters = []
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
+starty = 400
+for i in range(26):
+    x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
+    y = starty + ((i//13) * (GAP + RADIUS * 2))
+    letters.append([x, y, chr(65 + i), True])
+
+# game variables
+hangman_status = 0
+words = ["PYTHON", "PYGAME", "IDE", "REPLIT"]
+word = random.choice(words)
+guessed = []
 
 
-# Load images
-RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
-# Player ship
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
-# Lasers
-RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
-GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
-BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
-YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+def draw():
+    win.fill(WHITE)
+    # draw title
+    text = TITLE_FONT.render("DEVELOPER HANGMAN", 1, BLACK)
+    win.blit(text, (WIDTH/2 - text.get_width()/2, 20))
 
-# Background
-BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
-##################################################################
-class Laser:
-    def __init__(self, x, y, img):
-        self.x = x
-        self.y = y
-        self.img = img
-        self.mask = pygame.mask.from_surface(self.img)
-
-    def draw(self, window):
-        window.blit(self.img, (self.x, self.y))
-
-    def move(self, vel):
-        self.y += vel
-
-    def off_screen(self, height):
-        return not(self.y <= height and self.y >= 0)
-
-    def collision(self, obj):
-        return collide(obj, self)
-##################################################################
-class Ship: # Player Ship, Enemy Ship
-    COOLDOWN = 30
-
-    def __init__(self, x, y, color, health=100):
-        self.x = x
-        self.y = y
-        self.health = health
-        self.ship_img = None
-        self.laser_img = None
-        self.lasers = []
-        self.cool_down_counter = 0
+    # draw word
+    display_word =""
+    for letter in word:
+        if letter in guessed:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    text = WORD_FONT.render(display_word, 1, BLACK)
+    win.blit(text, (400, 200))
     
-    def draw(self, window):
-        window.blit(self.ship_img, (self.x, self.y))
-        for laser in self.lasers:
-            laser.draw(window)
 
-    def move_lasers(self, vel, obj):
-        self.cooldown()
-        for laser in self.lasers:
-            laser.move(vel)
-            if laser.off_screen(HEIGHT):
-                self.lasers.remove(laser)
-            elif laser.collision(obj):
-                obj.health -= 10
-                self.lasers.remove(laser)
+    # draw buttons
+    for letter in letters:
+        x, y, ltr, visible = letter
+        if visible:
+            pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+            text = LETTER_FONT.render(ltr, 1, BLACK)
+            win.blit(text, (x - text.get_width()/2, y- text.get_height()/2))
 
-    def cooldown(self):
-        if self.cool_down_counter >= self.COOLDOWN:
-            self.cool_down_counter = 0
-        elif self.cool_down_counter > 0:
-            self.cool_down_counter += 1   
+    win.blit(images[hangman_status], (150, 100))
+    pygame.display.update()
 
-    def shoot(self):
-        if self.cool_down_counter == 0:
-            laser = Laser(self.x, self.y, self.laser_img)
-            self.lasers.append(laser)
-            self.cool_down_counter = 1
-        
-    def get_width(self):
-        return self.ship_img.get_width()
-    
-    def get_height(self):
-        return self.ship_img.get_height()
-##################################################################
-class Player(Ship):
-    def __init__(self, x, y, health = 100):
-        super().__init__(x, y, health)
-        self.ship_img = YELLOW_SPACE_SHIP
-        self.laser_img = YELLOW_LASER
-        self.mask = pygame.mask.from_surface(self.ship_img)
-        self.max_health = health
+def display_message(message):
+    pygame.time.delay(1000)
+    win.fill(WHITE)
+    text = WORD_FONT.render(message, 1, BLACK)
+    win.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(3000)
 
-    def move_lasers(self, vel, objs):
-        self.cooldown()
-        for laser in self.lasers:
-            laser.move(vel)
-            if laser.off_screen(HEIGHT):
-                self.lasers.remove(laser)
-            else:
-                for obj in objs:
-                    if laser.collision(obj):
-                        objs.remove(obj)
-                        if laser in self.lasers:
-                            dself.lasers.remove(laser)
-
-    def draw(self,window):
-        super().draw(window)
-        self.healthbar(window)
-
-    def healthbar(self, window):
-        pygame.draw.rect(WIN, (255, 0, 0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 10))
-        pygame.draw.rect(WIN, (0, 255, 0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() * (self.health/ self.max_health), 10))
-
-##################################################################
-class Enemy(Ship):
-    COLOR_MAP = {
-                "red": (RED_SPACE_SHIP, RED_LASER),
-                "green": (GREEN_SPACE_SHIP, GREEN_LASER),
-                "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
-                }
-
-    def __init__(self, x, y, color, health= 100): 
-        super().__init__(x, y, health)
-        self.ship_img, self.laser_img = self.COLOR_MAP[color]
-        self.mask = pygame.mask.from_surface(self.ship_img)
-    
-    def move(self, vel):
-        self.y += vel
-    
-    def shoot(self):
-        if self.cool_down_counter == 0:
-            laser = Laser(self.x-20, self.y, self.laser_img)
-            self.lasers.append(laser)
-            self.cool_down_counter = 1
-##################################################################
-def collide(obj1, obj2):
-    offset_x = obj2.x - obj1.x
-    offset_y = obj2.y - obj1.y
-    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
-##################################################################
 def main():
-    run = True
+    global hangman_status
+
+    # set game loop
     FPS = 60
-    level = 0
-    lives = 5
-    main_font = pygame.font.SysFont("comicsans", 50)
-    lost_font = pygame.font.SysFont("comicsans", 50)
-
-    enemies = []
-    wave_length = 5
-    enemy_vel = 1
-    laser_vel = 5
-    player_vel = 5
-
-    player = Player(300, 630)
-
     clock = pygame.time.Clock()
-    
-    lost = False
-    lost_count = 0
-
-    def redraw_window():
-        WIN.blit(BG, (0,0))
-        # draw text
-        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255)) # f 는 안에 변수가 들어간다는 것을 나타낸다.
-        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
-
-        WIN.blit(lives_label, (10, 10))
-        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
-
-        for enemy in enemies:
-            enemy.draw(WIN)
-
-        player.draw(WIN)
-
-        if lost:
-            lost_label = lost_font.render("You Lost!!", 1, (255, 255, 255))
-            WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2 , 350))
-
-        pygame.display.update()
+    run = True
 
     while run:
         clock.tick(FPS)
-        redraw_window()
 
-        if lives <= 0 or player.health <= 0:
-            lost = True
-            lost_count += 1
-        
-        if lost:
-            if lost_count > FPS * 3:
-                run = False
-            else:
-                continue
-
-        if len(enemies) == 0:
-            level += 1
-            wave_length += 5
-            for i in range(wave_length):
-                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
-                enemies.append(enemy)
-                
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                quit()
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] and player.x - player_vel > 0: 
-            player.x -= player_vel
-        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH:
-            player.x += player_vel
-        if keys[pygame.K_w] and player.y - player_vel > 0:
-            player.y -= player_vel
-        if keys[pygame.K_s] and player.y + player_vel + player.get_height() +15 < HEIGHT:
-            player.y += player_vel
-        if keys[pygame.K_SPACE]:
-            player.shoot()
-
-        for enemy in enemies[:]:
-            enemy.move(enemy_vel)
-            enemy.move_lasers(laser_vel, player)
-
-            if random.randrange(0, 2*60) == 1:
-                enemy.shoot()
-
-            if collide(enemy, player):
-                player.health -= 10
-                enemies.remove(enemy)
-            elif enemy.y + enemy.get_height() > HEIGHT:
-                lives -= 1
-                enemies.remove(enemy)
-
-        player.move_lasers(-laser_vel, enemies)
-
-def main_menu():
-    title_font = pygame.font.SysFont("comicsans", 70)
-    run = True
-    while run:
-        WIN.blit(BG, (0,0))
-        title_label = title_font.render("Press the mouse to begin...", 1, (255, 255, 255))
-        WIN.blit(title_label, (WIDTH/2 -title_label.get_width()/2, 350))
-        pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                main()
-    
-    pygame.quit()
+                m_x, m_y = pygame.mouse.get_pos()
+                for letter in letters:
+                    x, y, ltr, visible = letter
+                    if visible:
+                        dis = math.sqrt((x- m_x)**2 + (y-m_y)**2) # m_x 가 x 보다 작은 값에 위치하고 있으면 -인데 부호를 제거하기 위해서
+                        if dis < RADIUS:
+                            letter[3] = False
+                            guessed.append(ltr)
+                            if ltr not in word:
+                                hangman_status += 1
 
+        draw()
 
-main_menu()
+        won = True
+        for letter in word:
+            if letter not in guessed:
+                won = False
+                break
+
+        if won:
+            display_message("YOU WON!")
+            break # loop를 나간다. = 게임종료
+
+        if hangman_status == 6:
+            display_message("YOU LOST")
+            break
+
+main()
+pygame.quit()
